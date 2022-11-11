120c09e59b373377a19177b88ba7cef9b6ef8cc2
adam-lim1 <adam.lim.1@gmail.com>
2022-11-11 16:52:48 -0600

    Implement token protected pages, stub login, redirect after login

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template, request, jsonify, make_response, redirect, url_for
 from functools import wraps
 import jwt
 from datetime import datetime, timedelta
@@ -27,29 +27,22 @@ def token_required(f):
             token = request.headers['x-access-token']
         print(token)
 
-        # return 401 if token is not passed
-        if not token:
-            return jsonify({'message' : 'Token is missing !!'}), 401
+        if not token: # No token provided
+            response = make_response(redirect(url_for('login')))
+            response.set_cookie('callback', url_for(f.__name__)) # set cookie to return to intended page
+            return response
   
         try:
             # decoding the payload to fetch the stored details
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-            
-            # Get user credentials from DB
-            if app.config['DEBUG'] == True:
-                current_user = data['public_id'] # Temporary - pass user once DB is set up
-            else:
-                pass
-                # # Get user from DB
-                # current_user = User.query\
-                #     .filter_by(public_id = data['public_id'])\
-                #     .first()
-        except:
-            return jsonify({
-                'message' : 'Token is invalid !!'
-            }), 401
+            userid = data['userid']
+        
+        except: # If not logged in, redirect
+            response = make_response(redirect(url_for('login')))
+            response.set_cookie('callback', url_for(f.__name__)) # set cookie to return to intended page
+            return response
         # returns the current logged in users context to the routes
-        return f(*args, **kwargs) # f(current_user, *args, **kwargs)
+        return f(userid, *args, **kwargs) # f(current_user, *args, **kwargs)
     return decorated
 
 @app.route('/login', methods =['POST', 'GET'])
@@ -57,24 +50,78 @@ def login():
     # Authenticate user, set token
     resp = None
 
-    # ToDo - functionality should be in User microservice
+    # Handle form input
+    if request.method == "POST":
+        username = request.form.get('email')
+        password = request.form.get('password')
 
-    if app.config['DEBUG'] == True:
-        # Create random token 
-        token = jwt.encode({'public_id':19, 'exp':datetime.utcnow() + timedelta(minutes=2)}, 'your secret key')
-        resp = make_response('Successfully Logged in and set cookie', 201)
-        resp.set_cookie('x-access-token', token)
+        # ToDo - validate with User Microservice and create token there
+        # Make POST to User login service
+        
+        # Dummy Function for Debugging - Create and set random token
+        if app.config['DEBUG'] == True: 
+            token = jwt.encode({'userid':19, 'exp':datetime.utcnow() + timedelta(minutes=2)}, app.config['SECRET_KEY'])
 
-    return resp
+        # Get redirect route from cookie
+        callback = request.cookies.get('callback')
+        if callback is None:
+            callback = url_for('index')
+        response = make_response(redirect(callback))
+        response.set_cookie('x-access-token', token)
+        return response # Response if token is valid
+
+    return render_template('login.html') # Response for GET
+
+## eBay Routes
 
 @app.route('/')
-@token_required
 def index():
-    return render_template('home.html')
+    if request.cookies.get('x-access-token') is None:
+        user = 'guest'
+    else:
+        user = request.cookies.get('x-access-token')
+    return render_template('home.html', user=user)
 
+@app.route('/cart')
+@token_required
+def viewCart(userid):
+    # ToDo - get data from Shopping microservice
+    # list of items and item info
+    return render_template('cart.html', user=userid)
+
+@app.route('/watchlist')
+@token_required
+def viewWatchlist(userid):
+    # ToDo - get data from Shopping microservice
+    # list of items and item info
+    return render_template('watchlist.html', user=userid)
+
+@app.route('/auction/<listing_id>')
+def viewAuction(listing_id=None):
+    # ToDo - get data from Auction microservice
+    # image, price, end time, details
+    return render_template('auction.html', listing_id=listing_id)
+
+@app.route('/create/auction')
+@token_required
+def createAuction():
+    return render_template('create_auction.html')
+
+@app.route('/create/account')
+def createAccount():
+    return render_template('create_account.html')
+
+# Update Account
+# Delete Account
+# Bid success landing page
+# Checkout
+# Admin - suspend/delete users
+# Admin - edit listings
+
+## Dummy routes for testing JWT
 @app.route('/protected')
 @token_required
-def protected():
+def protected(userid):
     print(request.cookies.get('x-access-token'))
     return 'Protected page'
 
@@ -82,7 +129,6 @@ def protected():
 def open():
     print(request.cookies.get('x-access-token'))
     return 'Open page'
-
 
 
 app.run(host='0.0.0.0', port=5000)
